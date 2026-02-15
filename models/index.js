212d9ed405a9b7ebb562c9bffc857a44c88e6cc2
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2026-02-15 18:26:47 +0530

    fix the model important error.

--- models/index.js
+++ models/index.js
@@ -3,12 +3,14 @@ import Store from './store.model.js';
 import Product from './product.model.js';
 import Inventory from './inventory.model.js';
 import User from './user.model.js';
+import Cart from './cart.model.js';
 
-export { Store, Product, Inventory, User };
+export { Store, Product, Inventory, User, Cart };
 
 export default {
     Store,
     Product,
     Inventory,
-    User
+    User,
+    Cart
 };
